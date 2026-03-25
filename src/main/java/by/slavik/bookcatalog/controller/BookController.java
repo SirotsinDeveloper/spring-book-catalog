8926943746d142351b4caeddc5aface894619c53
Sirotsin Viachaslau Konstantinovich <slavik.sirotin.02@gmail.com>
2026-03-25 21:43:21 +0300

    first commit

---
+++ src/main/java/by/slavik/bookcatalog/controller/BookController.java
@@ -0,0 +1,14 @@
+package by.slavik.bookcatalog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+}
---
+++ src/test/java/by/slavik/bookcatalog/BookCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package by.slavik.bookcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
